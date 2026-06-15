7799d4d8a868afac772e865cf590dee18d2f9181
Omkar Singh <frnk.kiyo007@gmail.com>
2026-06-15 16:42:16 +0530

    added include guard

--- socket.hpp
+++ socket.hpp
@@ -1,3 +1,7 @@
+#ifndef __SOCKET_HPP
+#define __SOCKET_HPP
+
+
 #include <sys/un.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -6,3 +10,6 @@
 constexpr char SV_SOCK_ADDR[] = "/tmp/sv_sock_addr";
 constexpr int MAXDATASIZE = 100;
 constexpr int BACKLOG = 10;
+
+
+#endif
